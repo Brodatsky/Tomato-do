420e91374ac0373c9ee023f94ad3ebb30b060e26
brokol1 <50028968+brokol1@users.noreply.github.com>
2020-11-29 13:48:11 +0300

    123

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,21 @@
 const path = require("path");
-const PAGES_DIR = `${PATHS.src}/pug/pages/`
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/js/index.js"],
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./index.js",
+  },
   output: {
-    filename: "bundle.js",
-    path: "path.resolve(__dirname, 'dist')"
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader'
-      }
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src/js"),
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: "env",
-          },
-        },
-      },
-    ],
-  },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.pug",
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
